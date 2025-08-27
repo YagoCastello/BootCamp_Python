161e2aa2d3662b00d7c0785882b49e964f5146a1
Yago Castello <yagodealmeidacs@gmail.com>
2025-08-26 23:02:31 -0300

    Atualizacao_Day1
    inputs, strings

---
+++ Days/Day_1_10/Day_1/ex1_3.py
@@ -0,0 +1,5 @@
+name = input("Qual é o seu nome? ")
+print("Oi " + name)
+#Outra forma de usar o print + input
+
+print("Hum... você tem "+input("Quantos anos você tem? ")+" anos")
---
+++ Days/Day_1_10/Day_1/ex1_5.py
@@ -0,0 +1,8 @@
+Tamanho_nome= len(input("Qual é o seu nome? "))
+print(Tamanho_nome)
+
+#ou
+
+username = input("Seu nome? ")
+length_username = len(username)
+print(length_username)
