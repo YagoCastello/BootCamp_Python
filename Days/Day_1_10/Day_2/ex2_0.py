491fa94d7fe6e4124d52ca77e58c5f288d3a6cb9
Yago Castello <yagodealmeidacs@gmail.com>
2025-08-28 14:39:44 -0300

    day2

---
+++ Days/Day_1_10/Day_2/ex2_0.py
@@ -0,0 +1,17 @@
+print("Hello"[4])
+
+#String
+print("123"+"456")
+
+#Printando numero grandes, facilitando a leitura de números grandes
+
+print(123_456_789)
+
+# float
+
+print(3.14159)
+
+# boolean
+
+print(True)
+print(False)
---
+++ Days/Day_1_10/Day_2/ex2_2.py
@@ -0,0 +1,12 @@
+print(1+2)
+print(2-1)
+print(2*3)
+print(6/9)
+print(6//9)
+print(6%9)
+print(3**3)
+
+print("-="*20)
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3 )/ 3 - 3)
